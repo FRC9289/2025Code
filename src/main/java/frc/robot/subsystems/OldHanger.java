39d00457cea4655a54121081b59f0e51c553ecc7
FRC9289 <frc9289@gmail.com>
2025-01-27 15:06:31 -0600

    Removed inefficiencies.

--- src/main/java/frc/robot/subsystems/OldHanger.java
+++ src/main/java/frc/robot/subsystems/OldHanger.java
@@ -4,7 +4,6 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class OldHanger extends SubsystemBase{
