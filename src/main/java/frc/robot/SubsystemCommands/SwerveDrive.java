94d744e94fbad4bed18541e855724ca45678221f
‘Jay <jayrajcorp@gmail.com>
2025-02-07 16:55:34 -0600

    did stuff for swerve drive need renaming and stuff

--- src/main/java/frc/robot/SubsystemCommands/SwerveDrive.java
+++ src/main/java/frc/robot/SubsystemCommands/SwerveDrive.java
@@ -1,83 +1,140 @@
-package frc.robot.SubsystemCommands;
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
 
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
-import com.revrobotics.CANSparkMax;
-import edu.wpi.first.wpilibj2.command.Command;
 
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.lib.drivers.PearadoxSparkMax;
+import frc.robot.Constants.SwerveConstants;
 
-public class SwerveDrive extends Command{
-    private Drivetrain drivetrain = drivetrain.getInstance();
+public class SwerveModule extends SubsystemBase {
+  private PearadoxSparkMax driveMotor;
+  private PearadoxSparkMax turnMotor;
 
-    public 
+  private RelativeEncoder driveEncoder;
+  private RelativeEncoder turnEncoder;
 
-    //x1 & y1 are the x and y axis from the strafing joystick
-    // x2 is the x axis from the rotation joystick
-    public static void drive(double x1, double y1, double x2){
-        // square root of the length + width squared
-        double r = Math.sqrt ((L * L) + (W * W));
-        //revsers the input of the y axis
-        y1 *=-1;
+  private PIDController turnPIDController;
+  private CANcoder absoluteEncoder;
 
-        //a, b, c, and d are the speeds of each of the 4 motors,   
-        //with this code (0->1), change as needed depending on the motors
-        double a = x1 - x2 * (L / r);
-        double b = x1 + x2 * (L / r);
-        double c = y1 - x2 * (W / r);
-        double d = y1 + x2 * (W / r);
+  private boolean absoluteEncoderReversed;
+  private double absoluteEncoderOffset;
 
-        //speeds
-        BRDMotor.set(Math.sqrt((a * a) + (d * d)));
-        BLDMotor.set(Math.sqrt((a * a) + (c * c)));
-        FRDMotor.set(Math.sqrt((b * b) + (d * d)));
-        FLDMotor.set(Math.sqrt((b * b) + (c * c)));
+  private int driveID = 0;
 
-        //Angle of each motor
-        FRTMotor.set(Math.atan2(a, d) / Math.PI);
-        BLTMotor.set(Math.atan2(a, c) / Math.PI);
-        FRTMotor.set(Math.atan2(b, d) / Math.PI);
-        FLTMotor.set(Math.atan2(b, c) / Math.PI);
+  private Rotation2d lastAngle;
 
-        SmartDashboard.putNumber("FLD", FLDMotor.get());
-        SmartDashboard.putNumber("FLT", FLTMotor.get());
-        SmartDashboard.putNumber("FRD", FRDMotor.get());
-        SmartDashboard.putNumber("FRT", FRTMotor.get());
-        SmartDashboard.putNumber("BLD", BLDMotor.get());
-        SmartDashboard.putNumber("BLT", BLTMotor.get());
-        SmartDashboard.putNumber("BRD", BRDMotor.get());
-        SmartDashboard.putNumber("BRT", BRTMotor.get());
-    }
-    public SwerveDrive(){
-        // //reset it every time -> to reset encosder specifically to initial settings
-        FLDMotor.restoreFactoryDefaults();
-        FLTMotor.restoreFactoryDefaults();
-        FRDMotor.restoreFactoryDefaults();
-        FRTMotor.restoreFactoryDefaults();
-        BLDMotor.restoreFactoryDefaults();
-        BLTMotor.restoreFactoryDefaults();
-        BRDMotor.restoreFactoryDefaults();
-        BRTMotor.restoreFactoryDefaults();
+  /** Creates a new SwerveModule. */
+  public SwerveModule(int driveMotorId, int turnMotorId, boolean driveMotorReversed, boolean turnMotorReversed,
+    int absoluteEncoderId, double absoluteEncoderOffset, boolean absoluteEncoderReversed) {
+      this.absoluteEncoderOffset = absoluteEncoderOffset;
+      this.absoluteEncoderReversed = absoluteEncoderReversed;
 
-        // //set the speed limit on how fast the motor can go
-        FLDMotor.setSmartCurrentLimit(MAX_SPEED);
-        FLTMotor.setSmartCurrentLimit(1);
-        FRDMotor.setSmartCurrentLimit(MAX_SPEED);
-        FRTMotor.setSmartCurrentLimit(1);
-        BLDMotor.setSmartCurrentLimit(MAX_SPEED);
-        BLTMotor.setSmartCurrentLimit(1);
-        BRDMotor.setSmartCurrentLimit(MAX_SPEED);
-        BRTMotor.setSmartCurrentLimit(1);
+      driveID = driveMotorId;
+      absoluteEncoder = new CANcoder(absoluteEncoderId);
 
-        // //saves changes to motor controller / encoder
-        FLDMotor.burnFlash();
-        FLTMotor.burnFlash();
-        FRDMotor.burnFlash();
-        FRTMotor.burnFlash();
-        BLDMotor.burnFlash();
-        BLTMotor.burnFlash();
-        BRDMotor.burnFlash();
-        BRTMotor.burnFlash();
+      driveMotor = new PearadoxSparkMax(driveMotorId, MotorType.kBrushless, IdleMode.kCoast, 45, driveMotorReversed);
+      turnMotor = new PearadoxSparkMax(turnMotorId, MotorType.kBrushless, IdleMode.kCoast, 25, turnMotorReversed);
 
-    }
+      driveEncoder = driveMotor.getEncoder();
+      turnEncoder = turnMotor.getEncoder();
+
+      turnPIDController = new PIDController(SwerveConstants.KP_TURNING, 0, 0);
+      turnPIDController.enableContinuousInput(-Math.PI, Math.PI);
+
+      resetEncoders();
+      lastAngle = getState().angle;
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    SmartDashboard.putNumber("Drive Distance (rot) - Motor: " + driveID, getDriveMotorPosition());
+    SmartDashboard.putNumber("Wheel Position (rot) - Motor: " + driveID, getTurnMotorPosition());
+    SmartDashboard.putNumber("Absolute Wheel Angle (deg) - Motor: " + driveID, absoluteEncoder.getAbsolutePosition().getValueAsDouble());
     
+  }
+
+  public void setBrake(boolean brake){
+    if(brake){
+      driveMotor.setIdleMode(IdleMode.kBrake);
+      turnMotor.setIdleMode(IdleMode.kCoast);
+    }
+    else{
+      driveMotor.setIdleMode(IdleMode.kCoast);
+      turnMotor.setIdleMode(IdleMode.kCoast);
+    }
+  }
+  
+  public double getDriveMotorPosition(){
+    return driveEncoder.getPosition() * SwerveConstants.DRIVE_MOTOR_PCONVERSION;
+  }
+
+  public double getDriveMotorVelocity(){
+    return driveEncoder.getVelocity() * SwerveConstants.DRIVE_MOTOR_VCONVERSION;
+  }
+
+  public double getTurnMotorPosition(){
+    return turnEncoder.getPosition() * SwerveConstants.TURN_MOTOR_PCONVERSION;
+  }
+
+  public double getTurnMotorVelocity(){
+    return turnEncoder.getVelocity() * SwerveConstants.TURN_MOTOR_VCONVERSION;
+  }
+
+  public double getAbsoluteEncoderAngle(){
+    double angle = absoluteEncoder.getAbsolutePosition().getValueAsDouble();
+    angle -= absoluteEncoderOffset;
+    angle *= (2 * Math.PI);
+    return angle * (absoluteEncoderReversed ? -1.0 : 1.0);
+  }
+
+  public void resetEncoders(){
+    driveEncoder.setPosition(0);
+    turnEncoder.setPosition(getAbsoluteEncoderAngle() / SwerveConstants.TURN_MOTOR_PCONVERSION);
+  }
+
+  public SwerveModuleState getState(){
+    return new SwerveModuleState(getDriveMotorVelocity(), new Rotation2d(getTurnMotorPosition()));
+  }
+
+  public SwerveModulePosition getPosition(){
+    return new SwerveModulePosition(getDriveMotorPosition(), new Rotation2d(getTurnMotorPosition()));
+  }
+
+  public void setDesiredState(SwerveModuleState desiredState){
+    desiredState = SwerveModuleState.optimize(desiredState, getState().angle); 
+    
+    setAngle(desiredState);
+    setSpeed(desiredState);
+    SmartDashboard.putString("Swerve [" + driveMotor.getDeviceId() + "] State", getState().toString());
+    SmartDashboard.putNumber("Abs Angle " + driveMotor.getDeviceId(), getAbsoluteEncoderAngle());
+  }
+
+  public void setSpeed(SwerveModuleState desiredState){
+    driveMotor.set(desiredState.speedMetersPerSecond / SwerveConstants.DRIVETRAIN_MAX_SPEED);
+  }
+
+  public void setAngle(SwerveModuleState desiredState){
+    Rotation2d angle = (Math.abs(desiredState.speedMetersPerSecond) <= (SwerveConstants.DRIVETRAIN_MAX_SPEED * 0.01)) ? lastAngle : desiredState.angle; //Prevent rotating module if speed is less then 1%. Prevents Jittering.
+    
+    turnMotor.set(turnPIDController.calculate(getTurnMotorPosition(), desiredState.angle.getRadians()));
+    lastAngle = angle;
+  }
+
+  public void stop(){
+    driveMotor.set(0);
+    turnMotor.set(0);
+  }
 }
