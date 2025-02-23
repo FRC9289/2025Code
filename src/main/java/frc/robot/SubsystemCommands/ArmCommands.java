bf4d4cfad2eff043d3c0b151bed6374eac8b6221
FRC9289 <frc9289@gmail.com>
2025-02-23 16:20:40 -0600

    Arm Done

---
+++ src/main/java/frc/robot/SubsystemCommands/ArmCommands.java
@@ -0,0 +1,40 @@
+package frc.robot.SubsystemCommands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Arm;
+import edu.wpi.first.wpilibj.Timer;
+
+public class ArmCommands extends Command {
+    // Define Motor Attributes
+    Arm armMotor;
+    double power;
+    boolean up;
+    Timer timer = new Timer();
+    double mtime = 2;
+
+    public ArmCommands(Arm _armMotor, double speed) {
+        this.armMotor = _armMotor;
+        this.power = speed;
+        this.up = false;
+        addRequirements(armMotor);
+    }
+
+    public void up() {
+        if (up) {
+            timer.reset();
+            while (timer.get() < mtime) {
+                armMotor.move(this.power);
+            }
+        }
+    }
+
+    public void down() {
+        if (!up) {
+            timer.reset();
+            while (timer.get() < mtime) {
+                armMotor.move(-1 * this.power);
+            }
+        }
+    }
+}
+// Written by Wolfram121
