537e4daa5688425053bb398dd43411d3dbcfef64
Aditya-2204 <william1binki@gmail.com>
2025-02-23 18:21:05 -0600

    Removed gunk & Switched Arm subsystem to CAN

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -1,8 +1,7 @@
 package frc.robot.subsystems;
 
-import frc.robot.lib.drivers.VikingSparkMax;
+import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
-import com.revrobotics.CANSparkBase.IdleMode;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -10,8 +9,8 @@ public class Arm extends SubsystemBase {
     private final int speedLimit = 20;
 
     // Provide all required parameters
-    private VikingSparkMax arm1 = new VikingSparkMax(16, MotorType.kBrushless, IdleMode.kBrake, speedLimit, false);
-    private VikingSparkMax arm2 = new VikingSparkMax(17, MotorType.kBrushed, IdleMode.kBrake, speedLimit, false); //Constructor showing error, so provided all parameters
+    private CANSparkMax arm1 = new CANSparkMax(16,MotorType.kBrushed);
+    private CANSparkMax arm2 = new CANSparkMax(17, MotorType.kBrushed); //Constructor showing error, so provided all parameters
 
     public Arm() {
         arm1.restoreFactoryDefaults();
