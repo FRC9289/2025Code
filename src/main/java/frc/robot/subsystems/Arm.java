13dd8f07f63fcf2587b9f699ba8ed8eb78d77941
Wolfram121 <avi.baj@outlook.com>
2025-02-24 18:58:34 -0600

    Fixed Arm to only 1 arm Motor.

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -9,39 +9,29 @@ public class Arm extends SubsystemBase {
     private final int speedLimit = 20;
 
     // Provide all required parameters
-    private CANSparkMax arm1 = new CANSparkMax(16,MotorType.kBrushless);
-    private CANSparkMax arm2 = new CANSparkMax(17, MotorType.kBrushless); //Constructor showing error, so provided all parameters
+    private CANSparkMax arm1 = new CANSparkMax(9,MotorType.kBrushless);
 
     public Arm() {
         arm1.restoreFactoryDefaults();
-        arm2.restoreFactoryDefaults();
 
         arm1.setSmartCurrentLimit(speedLimit);
-        arm2.setSmartCurrentLimit(speedLimit);
 
         arm1.burnFlash();
-        arm2.burnFlash();
     }
 
     public void move(double speed) {
         arm1.set(speed);
-        arm2.set(-1 * speed);
         SmartDashboard.putNumber("Arm Motor 1", arm1.get());
-        SmartDashboard.putNumber("Arm Motor 2", arm2.get());
     }
 
     public void stop() {
         arm1.set(0);
-        arm2.set(0);
     }
 
     @Override
     public void periodic() {
         SmartDashboard.putNumber("Arm Motor 1", arm1.get());
-        SmartDashboard.putNumber("Arm Motor 2", arm2.get());
     }
 }
 
-/*created by Wolfram
- * Edited by Aditya-2204
-*/
+//Wolfram
